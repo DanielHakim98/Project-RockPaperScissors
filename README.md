f6955ffb7bec59405121b850ec6bfab4f481acaf
Daniel Hakim <daniel.hakim.jamaluddin@gmail.com>
2021-09-19 16:54:55 +0800

    Some improvement on the script and please read README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+This is my first project with Javascript. After years left programming in C and C++, finally I'm actively doing programming project with Javascript. Well at least it's less strict compare to C/C++.
+Here is the code for Rock, Paper and Scissors.
+So here's my update on the project.
+
+19 Sep 21:
+-Bascially, the code run really well, no bugs as for now but surely there are room for improvement.
+
+-One of them are the prompt is not case sensitive. The inputs are restricted to "rock", "paper and "scissors". Other than those, even with small Upper case typo, the program will loop ask request the user to insert the correct value.
+
+-The second is using 'var' for global variable which some people say is a big no no. Because it has been replaced by 'let' and 'const'.  I use it to count how many wins both player and conm have. I'm going to do slight tweaak on this issue so I won't need to use 'var'.
--- js/game.js
+++ js/game.js
@@ -1,45 +1,99 @@
-function playerInput(){
+function game(){
+    function playerInput(){  /*Function for receiving input from user and return back to main function*/
     
-    let playerSelection;
-
-    do
-    {
-        playerSelection=prompt('Choose "rock","paper" or "scissors":'); 
-
-        if(playerSelection==="rock")return playerSelection;
-        else if(playerSelection==="paper")return playerSelection;
-        else if(playerSelection==="scissors")return playerSelection;
-
+        let playerSelection;
     
-    } while( playerSelection==="" || playerSelection!=="rock" || playerSelection!=="paper" || playerSelection!=="scissors");
-    return playerSelection;
-}
-
-function playRound(playerSelection, computerSelection){
-    if(playerSelection==="rock")
-    {
-        if(computerSelection==="rock")
-        {console.log("It's a tie");}
-
-        else if(computerSelection==="paper")
-        {console.log("You lose!");}
-
-        else
-        {console.log("Congrats! You win!");}
+        do
+        {
+            playerSelection=prompt('Choose "rock","paper" or "scissors":'); 
+    
+            if(playerSelection==="rock")return playerSelection;
+            else if(playerSelection==="paper")return playerSelection;
+            else if(playerSelection==="scissors")return playerSelection;
+    
+        
+        } while( playerSelection==="" || playerSelection!=="rock" || playerSelection!=="paper" || playerSelection!=="scissors");
+        return playerSelection;
     }
-
+    
+    function playRound(playerSelection, computerSelection){ /*Function for checking who wins, you or the processors behind the screeen*/
+        if(playerSelection==="rock")
+        {   switch(computerSelection){
+    
+            case "rock":
+                console.log("It's a tie");
+                break;
+            case "paper":
+                console.log("You lose! Poor you");
+                ++comCounter;
+                break;
+            default:
+                console.log("Congrats! You win!");
+                ++playerCounter;
+                break;
+            }
+        }
+    
+        else if(playerSelection==="paper")
+        {   switch(computerSelection){
+    
+            case "paper":
+                console.log("It's a tie");
+                break;
+            case "scissors":
+                console.log("You lose! Poor you");
+                ++comCounter;
+                break;
+            default:
+                console.log("Congrats! You win!");
+                ++playerCounter;
+                break;
+            }
+        }
+    
+        else 
+        {   switch(computerSelection){
+    
+            case "scissors":
+                console.log("It's a tie");
+                break;
+            case "rock":
+                console.log("You lose! Poor you");
+                ++comCounter;
+                break;
+            default:
+                console.log("Congrats! You win!");
+                ++playerCounter;
+                break;
+            }
+        }
+    }
+    
+    function computerPlay(){            /*Function for randomizing the selection made by the COM*/
+        let randomNum = Math.floor(Math.random()*(9-1+1))+1;
+        let randomAns = (randomNum == 1|| randomNum==5 || randomNum==9)?"rock"
+        :(randomNum==2||randomNum==6||randomNum==7)?"paper"
+        :"scissors";
+        return randomAns;
+    }
+    
+    let playerSelection=playerInput();
+    console.log("You selected: ", playerSelection);
+    const computerSelection = computerPlay();
+    console.log("COM selected: ", computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    
 }
 
-function computerPlay(){
-    let randomNum = Math.floor(Math.random()*(9-1+1))+1;
-    let randomAns = (randomNum == 1|| randomNum==5 || randomNum==9)?"rock"
-    :(randomNum==2||randomNum==6||randomNum==7)?"paper"
-    :"scissors";
-    return randomAns;
-}
+var playerCounter = 0;
+var comCounter    = 0;   
+    do{
+        
+        game();
+        console.log(`Player: ${playerCounter}, COM: ${comCounter}`);
+        if(playerCounter==5)console.log("Ahoi! You're the absolutey winner");
+        else if(comCounter==5) console.log("Too bad you lose. Don't worry try again next time");
+    
 
-let playerSelection=playerInput();
-console.log("You selected: ", playerSelection);
-const computerSelection = computerPlay();
-console.log("COM selected: ", computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+    }while(playerCounter<5 && comCounter<5);
+    
